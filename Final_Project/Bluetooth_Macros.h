43d9e8db73b35add857b70dd797fb5ebbd598591
Nino Maruszewski <nino.maruszewski@gmail.com>
2020-01-18 17:56:52 -0600

    Fixed macros, also moved to right location

--- Final_Project/Bluetooth_Macros.h
+++ Final_Project/Bluetooth_Macros.h
@@ -18,14 +18,14 @@
 #define STOP 'S'
 
 // Speeds
-#define SPEED0 0
-#define SPEED1 1
-#define SPEED2 2
-#define SPEED3 3
-#define SPEED4 4
-#define SPEED5 5
-#define SPEED6 6
-#define SPEED7 7
-#define SPEED8 8
-#define SPEED9 9
+#define SPEED0 '0'
+#define SPEED1 '1'
+#define SPEED2 '2'
+#define SPEED3 '3'
+#define SPEED4 '4'
+#define SPEED5 '5'
+#define SPEED6 '6'
+#define SPEED7 '7'
+#define SPEED8 '8
+#define SPEED9 '9'
 #define SPEED10 'q'
